5e3553db6f41617c4a609e73de4f2afedc5727af
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-02-02 00:09:15 +0100

    feat: cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 app.use(express.json());
+app.use(cors());
 
 const { getMCSRStreams } = require("./controllers/mcsr");
 
